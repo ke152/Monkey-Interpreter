17f9348caf562854ce15e00f920c2f2f73b480a8
ke152 <ke152@foxmail.com>
2022-11-17 22:58:50 +0800

    1.2-定义词法单元

---
+++ src/token.cs
@@ -0,0 +1,27 @@
+﻿internal struct Token
+{
+    public TokenType Type;
+    public string Literal;
+}
+
+enum TokenType
+{
+    ILLEGAL,//非法
+    EOF,//文件结束
+    // 标识符+字面量
+    IDENT,// add, foobar, x, y, ... 
+    INT,// 1343456 
+    // 运算符
+    ASSIGN,
+    PLUS,
+    // 分隔符
+    COMMA,//逗号
+    SEMICOLON,//分号
+    LPAREN,//左小括号
+    RPAREN,//右小括号
+    LBRACE,//左花括号
+    RBRACE,//右花括号
+    // 关键字
+    FUNCTION,
+    LET,
+}
