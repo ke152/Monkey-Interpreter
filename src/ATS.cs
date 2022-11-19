42c96ce472d8c863857be6df982bcc02b7b87707
ke152 <ke152@foxmail.com>
2022-11-20 00:01:47 +0800

    2.4-解析let语句

---
+++ src/ATS.cs
@@ -0,0 +1,93 @@
+﻿interface INode
+{
+    string TokenLiteral();
+    string OutLine();
+}
+
+interface IStatement : INode
+{
+    void StatementNode();
+}
+
+interface IExpression : INode
+{
+    void ExpressionNode();
+}
+
+class Identifier : IExpression
+{
+    public Token Token { get; set; }
+    public Identifier Name { get; set; }
+    public string Value { get; set; }
+    public void ExpressionNode()
+    {
+
+    }
+
+    public string OutLine()
+    {
+        var str = $"{Value}";
+        return str;
+    }
+
+    public string TokenLiteral()
+    {
+        return Token.Literal;
+    }
+}
+
+class LetStatement : IStatement
+{
+    public Token Token { get; set; }
+    public Identifier Name { get; set; }
+    public IExpression Value { get; set; }
+
+    public string OutLine()
+    {
+        string str = $"{TokenLiteral()}  {Name.OutLine()} ={Value?.OutLine()};";
+        return str;
+    }
+
+    public void StatementNode()
+    {
+
+    }
+
+    public string TokenLiteral()
+    {
+
+        return Token.Literal;
+    }
+
+    
+}
+
+internal class AtsProgram : INode
+{
+    public AtsProgram()
+    {
+        Statement = new List<IStatement>();
+    }
+    public List<IStatement> Statement { get; set; }
+
+    public string OutLine()
+    {
+        string str = string.Empty;
+        foreach (var item in Statement)
+        {
+            str += item.OutLine();
+        }
+        return str;
+    }
+
+    public string TokenLiteral()
+    {
+        if (Statement.Count > 0)
+        {
+            return Statement[0].TokenLiteral();
+        }
+        return "";
+    }
+}
+
+
---
+++ src/Parser.cs
@@ -0,0 +1,125 @@
+﻿internal class Parser {
+	public Lexer Lexer;
+	public Token CurToken = new();
+	public Token PeekToken = new();
+	public List<string> Errors = new();
+
+	public Parser(Lexer l) {
+		this.Lexer = l;
+
+		NextToken();
+		NextToken();
+	}
+
+
+	public void NextToken()
+	{
+		this.CurToken = this.PeekToken;
+		this.PeekToken = this.Lexer.NextToken();
+	}
+
+	public AtsProgram ParseProgram() {
+		AtsProgram program = new();
+
+		while (!CurTokenIs(TokenType.EOF))
+		{
+			var stmt = ParserStatement();
+			if (stmt != null)
+			{
+				program.Statement.Add(stmt);
+			}
+			NextToken();
+		}
+		return program;
+	}
+
+	public IStatement? ParserStatement()
+	{
+		IStatement? statement = null;
+		switch (CurToken.Type)
+		{
+			case TokenType.LET:
+				statement = ParserLetStatement();
+				break;
+			case TokenType.RETURN:
+				statement = ParserRuturnStatement();
+				break;
+			default:
+				statement = ParseExpressionStatement();
+				break;
+		}
+		return statement;
+	}
+
+	public LetStatement? ParserLetStatement()
+	{
+		var stmt = new LetStatement() { Token = CurToken };
+		if (!ExpectPeek(TokenType.IDENT))
+		{
+			//CurError(stmt);
+			//PeekError(TokenEnum.IDENT);
+			return null;
+		}
+		stmt.Name = new Identifier() { Token = CurToken, Value = CurToken.Literal };
+		if (!ExpectPeek(TokenType.ASSIGN))
+		{
+			//CurError(stmt);
+			//PeekError(TokenEnum.ASSIGN);
+			return null;
+		}
+		//NextToken();
+        //stmt.Value = ParserExpression();
+        //if (PeekTokenIs(TokenEnum.SEMICOLON))
+        //{
+        //	NextToken();
+        //}
+        while (!CurTokenIs(TokenType.SEMICOLON))
+        {
+            NextToken();
+        }
+        return stmt;
+	}
+
+	public bool PeekTokenIs(TokenType token)
+	{
+		return PeekToken?.Type == token;
+	}
+
+	public bool CurTokenIs(TokenType token)
+	{
+		return CurToken?.Type == token;
+	}
+
+	public bool ExpectPeek(TokenType token)
+	{
+		if (PeekTokenIs(token))
+		{
+			NextToken();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public void PeekError(TokenType token)
+	{
+		var error = $"expected next token to be {token}, got {PeekToken.Type} instead ";
+		Console.WriteLine(error);
+		Errors.Add(error);
+	}
+
+	public bool ExpectPeek(TokenType token)
+	{
+		if (PeekTokenIs(token))
+		{
+			NextToken();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
