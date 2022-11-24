2ad4201838a2709b7a22f4fbd6f62632056d8d89
ke152 <ke152@foxmail.com>
2022-11-24 10:40:19 +0800

    3.4-表示对象

---
+++ src/MonkeyObject.cs
@@ -0,0 +1,43 @@
+﻿internal interface IMonkeyObject
+{
+    internal string Inspect();
+}
+
+internal enum MonkeyObjectType
+{
+    INTEGER,
+    Boolean,
+    Null,
+}
+
+internal class MonkeyInteger : IMonkeyObject
+{
+    public MonkeyObjectType Type = MonkeyObjectType.INTEGER;
+
+    public int Value;
+    public string Inspect()
+    {
+        return Value.ToString();
+    }
+}
+
+internal class MonkeyBoolean : IMonkeyObject
+{
+    public MonkeyObjectType Type = MonkeyObjectType.Boolean;
+
+    public bool Value;
+    public string Inspect()
+    {
+        return Value.ToString();
+    }
+}
+
+internal class MonkeyNull : IMonkeyObject
+{
+    public MonkeyObjectType Type = MonkeyObjectType.Null;
+
+    public string Inspect()
+    {
+        return "null";
+    }
+}
